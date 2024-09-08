0b2c4bdabf7c09745fb77a1f7ed1f03a9535e633
Lilyhouser <houselily010@gmail.com>
2024-09-08 22:41:20 +0700

    rac met moi

--- GameCodeFest2024/GameCodeFest2024/src/Main.java
+++ GameCodeFest2024/GameCodeFest2024/src/Main.java
@@ -3,21 +3,19 @@ import jsclub.codefest2024.sdk.Hero;
 import jsclub.codefest2024.sdk.algorithm.PathUtils;
 import jsclub.codefest2024.sdk.base.Node;
 import jsclub.codefest2024.sdk.model.GameMap;
-import jsclub.codefest2024.sdk.model.enemies.Enemy;
 import jsclub.codefest2024.sdk.model.obstacles.Obstacle;
 import jsclub.codefest2024.sdk.model.players.Player;
 import jsclub.codefest2024.sdk.model.weapon.Weapon;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     private static final String SERVER_URL = "https://cf-server.jsclub.dev";
-    private static final String GAME_ID = "179403";
-    private static final String PLAYER_NAME = "lilip";
+    private static final String GAME_ID = "170043";
+    private static final String PLAYER_NAME = "IDontKnowCode";
 
     public static double calDistance(Node x, Node y){
         return Math.sqrt(Math.pow(x.getX()-y.getX(),2) + Math.pow(x.getY()-y.getY(),2));
@@ -52,6 +50,21 @@ public class Main {
         return enemyList;
     }
 
+    public static List<Obstacle> sortChest(List<Obstacle> chestList, Player player){
+        chestList.sort(new Comparator<Obstacle>() {
+            @Override
+            public int compare(Obstacle o1, Obstacle o2) {
+                if(calDistance(player, o1) < calDistance(player, o2)){
+                    return -1;
+                }else if(calDistance(player, o1) >= calDistance(player, o2)){
+                    return 1;
+                }
+                return 0;
+            }
+        });
+        return chestList;
+    }
+
     public static void main(String[] args) throws IOException {
         Hero hero = new Hero(GAME_ID, PLAYER_NAME);
 
@@ -77,9 +90,55 @@ public class Main {
                     }
 
                     Weapon currentWeapon = hero.getInventory().getGun();
-                    if(currentWeapon !=null) System.out.println("ID: " + currentWeapon.getId());
 
-                    if (currentWeapon == null){
+                    if (currentWeapon!=null){
+                        System.out.println("ID: " + currentWeapon.getId());
+                        final List<Player> enemies = sortEnemy(gameMap.getOtherPlayerInfo(), player);
+                        int i = 0;
+                        Player currentEnemy = enemies.get(i);
+                        while(!enemies.get(i).getIsAlive()){
+                            i++;
+                            currentEnemy = enemies.get(i);
+                        }
+
+                        final List<Obstacle> chestList = sortChest(gameMap.getListChests(), player);
+                        Node targetChest = chestList.getFirst();
+
+
+                        System.out.println("Current Enemy: "+ currentEnemy.getPlayerName());
+                        Node target = new Node(currentEnemy.getX(), currentEnemy.getY());
+                        int dx = player.getX() - target.getX();
+                        int dy = player.getY() - target.getY();
+
+                        if((target.getX() == player.getX() && calDistance(target, player) <= currentWeapon.getRange())
+                        || (target.getY() == player.getY() && calDistance(target, player) <= currentWeapon.getRange())){
+                            if(dx > 0){
+                                hero.shoot("l");
+                            }else if (dx < 0){
+                                hero.shoot("r");
+                            }else if(dy > 0){
+                                hero.shoot("d");
+                            }else if (dy < 0){
+                                hero.shoot("u");
+                            }
+                        } else if ((targetChest.getX()==player.getX() && calDistance(targetChest, player) <= currentWeapon.getRange())
+                                || (targetChest.getY()==player.getY() && calDistance(targetChest, player) <= currentWeapon.getRange())){
+                            if(dx > 0){
+                                hero.shoot("l");
+                            }else if (dx < 0){
+                                hero.shoot("r");
+                            }else if(dy > 0){
+                                hero.shoot("d");
+                            }else if (dy < 0){
+                                hero.shoot("u");
+                            }
+                        } else{
+                            String path = PathUtils.getShortestPath(gameMap, restrictedPoints, player, target, false);
+                            hero.move(path);
+                            System.out.println("Path: "+path);
+                        }
+                    }
+                    else {
                         final List<Weapon> gunList = sortGunList(gameMap.getAllGun(), player);
                         Weapon targetWeapon = gunList.getFirst();
                         System.out.println("Target: "+targetWeapon.getId());
@@ -90,39 +149,9 @@ public class Main {
                             String path = PathUtils.getShortestPath(gameMap, restrictedPoints, player, targetWeapon, false);
                             hero.move(path);
                             System.out.println("Path: "+path);
-                            hero.move(path);
                         }
                     }
-                    if (currentWeapon != null){
-                        final List<Player> enemies = sortEnemy(gameMap.getOtherPlayerInfo(), player);
-                        int i = 0;
-                        Player currentEnemy = enemies.get(i);
-                        while(!enemies.get(i).getIsAlive()){
-                            i++;
-                            currentEnemy = enemies.get(i);
-                        }
-                        System.out.println("Current Enemy: "+ currentEnemy.getPlayerName());
-                        Node target = new Node(currentEnemy.getX(), currentEnemy.getY());
-                        int dx = player.getX() - target.getX();
-                        int dy = player.getY() - target.getY();
-                        if((Math.abs(dx) <= currentWeapon.getRange()) && (player.getY() == target.getY())){
-                            if(dx > 0){
-                                hero.shoot("l");
-                            }else{
-                                hero.shoot("r");
-                            }
-                        }else if(Math.abs(dy) <= currentWeapon.getRange() && (player.getX() == target.getX())){
-                            if(dy > 0){
-                                hero.shoot("d");
-                            }else{
-                                hero.shoot("u");
-                            }
-                        }else{
-                            String path = PathUtils.getShortestPath(gameMap, restrictedPoints, player, target, false);
-                            hero.move(path);
-                            System.out.println("Path: "+path);
-                        }
-                    }
+
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
