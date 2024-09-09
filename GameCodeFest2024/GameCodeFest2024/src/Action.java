1d54e8d36c39ad4ff948ca523558754f59f41e9f
Tran Dang Viet Huy <viethuy2904@gmail.com>
2024-09-09 11:18:09 +0700

    group all sortPlayer, sortWeapon, etc into sortElement function

--- GameCodeFest2024/GameCodeFest2024/src/Action.java
+++ GameCodeFest2024/GameCodeFest2024/src/Action.java
@@ -6,11 +6,12 @@ import jsclub.codefest2024.sdk.model.GameMap;
 import jsclub.codefest2024.sdk.model.obstacles.Obstacle;
 import jsclub.codefest2024.sdk.model.players.Player;
 import jsclub.codefest2024.sdk.model.weapon.Weapon;
+import jsclub.codefest2024.sdk.model.Element;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
+
 
 public class Action {
     public static String actionAttack(Node a, Node b,Weapon currentWeapon) {
@@ -38,5 +39,29 @@ public class Action {
         Weapon melee = meleeList.getFirst();
         String movement = PathUtils.getShortestPath(gameMap, restrictedPoints, player, melee, false);
         System.out.println(movement);*/
+    }
+    // CALCULATE DISTANCE BETWEEN TWO OBJECT
+    public static double calDistance(Node x, Node y){
+        return Math.sqrt(Math.pow(x.getX()-y.getX(),2) + Math.pow(x.getY()-y.getY(),2));
+    }
+
+
+    // PUT THE CLOSEST ELEMENT TO THE MAIN PLAYER ON THE TOP OF THE LIST
+    public static <T extends Element> List <T> sortElement(List<T> elementList, Player player){
+
+        Comparator <T> compare = new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                if(Action.calDistance(player,o1) < Action.calDistance(player,o2))
+                    return -1;
+                else if(Action.calDistance(player,o1) < Action.calDistance(player,o2))
+                    return 0;
+                else
+                    return 1;
+            }
+        };
+
+        elementList.sort(compare);
+        return elementList;
     }
 }
--- GameCodeFest2024/GameCodeFest2024/src/Main.java
+++ GameCodeFest2024/GameCodeFest2024/src/Main.java
@@ -14,57 +14,9 @@ import java.util.List;
 
 public class Main {
     private static final String SERVER_URL = "https://cf-server.jsclub.dev";
-    private static final String GAME_ID = "112792";
+    private static final String GAME_ID = "160974";
     private static final String PLAYER_NAME = "Retard";
 
-    public static double calDistance(Node x, Node y){
-        return Math.sqrt(Math.pow(x.getX()-y.getX(),2) + Math.pow(x.getY()-y.getY(),2));
-    }
-
-    public static List<Weapon> sortGunList(List<Weapon> gunList, Player player){
-        gunList.sort(new Comparator<Weapon>() {
-            @Override
-            public int compare(Weapon o1, Weapon o2) {
-                if(calDistance(player, o1) < calDistance(player, o2)){
-                    return -1;
-                }else if(calDistance(player, o1) >= calDistance(player, o2)){
-                    return 1;
-                }
-                return 0;
-
-            }
-        });
-        return gunList;
-    }
-
-    public static List<Player> sortPlayer(List<Player> players, Player player){
-        Comparator <Player> compare = new Comparator<Player>() {
-            @Override
-            public int compare(Player o1, Player o2) {
-                if(calDistance(player,o1) < calDistance(player,o2))
-                    return -1;
-                else if (calDistance(player,o1) >= calDistance(player,o2))
-                    return 1;
-                return 0;
-            }
-        };
-        players.sort(compare);
-        return players;
-    }
-    public static List<Obstacle> sortChest(List<Obstacle> chest, Node player){
-        Comparator <Obstacle> compare = new Comparator<Obstacle>() {
-            @Override
-            public int compare(Obstacle o1, Obstacle o2) {
-                if(calDistance(player,o1) < calDistance(player,o2))
-                    return -1;
-                else if (calDistance(player,o1) >= calDistance(player,o2))
-                    return 1;
-                return 0;
-            }
-        };
-        chest.sort(compare);
-        return chest;
-    }
 
     public static void main(String[] args) throws IOException {
         Hero hero = new Hero(GAME_ID, PLAYER_NAME);
@@ -89,17 +41,18 @@ public class Main {
                     for(Obstacle o : restrictedList){
                         restrictedPoints.add(new Node(o.getX(), o.getY()));
                     }
+//================================================================================================================
 
                     Weapon currentGun = hero.getInventory().getGun();
                     Weapon currentMelee = hero.getInventory().getMelee();
-
                     Weapon currentThrowAble = hero.getInventory().getThrowable();
                     List<Weapon> meleeList = gameMap.getAllMelee();
                     List<Weapon> throwableList = gameMap.getAllThrowable();
+
                     if(currentThrowAble !=null) System.out.println("ID: " + currentThrowAble.getId());
 
                     if (currentGun == null){
-                        final List<Weapon> gunList = sortGunList(gameMap.getAllGun(), player);
+                        final List<Weapon> gunList = Action.sortElement(gameMap.getAllGun(), player);
                         Weapon targetWeapon = gunList.getFirst();
 
                         if(player.getX()==targetWeapon.getX() && player.getY()==targetWeapon.getY()){
@@ -111,56 +64,46 @@ public class Main {
 
                             ;
                         }
-                    }else if(currentGun != null){
-                        List<Player>others = sortPlayer(otherPlayers, player);
+                    }else if(currentGun != null || currentMelee != null || currentThrowAble != null){
+                        final List<Player>others = Action.sortElement(otherPlayers, player);
                         if(others.getFirst().getIsAlive() == false || others.getFirst().getHp() == 0)
                             others.remove(others.getFirst());
 
-                        final Player targetPlayer = others.getFirst();
+                         Player targetPlayer = others.getFirst();
                         Node target = new Node(targetPlayer.x,targetPlayer.y);
 
-                        if(player.getX()== targetPlayer.x){
-                            System.out.print(player.getX());
-                            if(player.getY() <= targetPlayer.y + currentGun.getRange() && player.getY() > targetPlayer.y )
-                                hero.shoot("d");
+                        hero.shoot(Action.actionAttack(player, targetPlayer, currentGun));
 
-                            else if(player.getY() >= targetPlayer.y - currentGun.getRange() && player.getY() < targetPlayer.y)
-                                hero.shoot("u");
 
-                        }else if(player.getY() == targetPlayer.y ){
-                            System.out.print(currentMelee.getId());
-                            if(player.getX()  <= targetPlayer.x + currentGun.getRange() && player.getX() > targetPlayer.x)
-                                hero.shoot("l");
-
-                            else if(player.getX() >= targetPlayer.x - currentGun.getRange() && player.getX() < targetPlayer.x )
-                                hero.shoot("r");
-
-                        }
-                            final List<Obstacle> chestList = sortChest(gameMap.getListChests(), player);
+                            final List<Obstacle> chestList = Action.sortElement(gameMap.getListChests(), player);
                             Obstacle chest = chestList.getFirst();
                             restrictedPoints.addAll(restrictedList);
                             String movement;
-                            if(calDistance(player,chest) <= 3.0 ) {
+                            if(Action.calDistance(player,chest) <= 3.0 ) {
                                 hero.shoot(Action.actionAttack(player, chest, currentGun));
 
 
                             }
-                            if(currentMelee.getId() == "HAND" && !meleeList.isEmpty() && calDistance(player,meleeList.getFirst()) <= 3.0){
+                            if(currentMelee.getId() == "HAND" && !meleeList.isEmpty() && Action.calDistance(player,meleeList.getFirst()) <= 3.0){
 
                             Weapon melee = meleeList.getFirst();
                             movement = PathUtils.getShortestPath(gameMap, restrictedPoints, player, melee, false);
+                            hero.move(movement);
                             System.out.println(movement);
-                            }else if(currentThrowAble == null && !throwableList.isEmpty() && calDistance(player,throwableList.getFirst()) <= 3.0) {
+                            hero.pickupItem();
+                            }/*else if(currentThrowAble == null && !throwableList.isEmpty() && calDistance(player,throwableList.getFirst()) <= 3.0) {
 
-                            Weapon throwable = throwableList.getFirst();
-                            movement = PathUtils.getShortestPath(gameMap, restrictedPoints, player, throwable, false);
-                            System.out.println(movement);
-                            }
+                                Weapon throwable = throwableList.getFirst();
+                                movement = PathUtils.getShortestPath(gameMap, restrictedPoints, player, throwable, false);
+                                hero.move(movement);
+                                System.out.println(movement);
+                                hero.pickupItem();
+                            }*/else {
 
 
                                 movement = PathUtils.getShortestPath(gameMap, restrictedPoints, player, target, false);
 
-
+                                }
                                 hero.move(movement);
 
 
